42373d4c78e577385728fad3eb445540228f5a1b
Jiangchuan <jiangchuan.huang@gmail.com>
2017-05-19 15:59:51 -0500

    gRPC to Start With

---
+++ app/src/main/java/io/chizi/tickethare/MainActivity.java
@@ -0,0 +1,100 @@
+package io.chizi.tickethare;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mSendButton;
+    private EditText mHostEdit;
+    private EditText mPortEdit;
+    private EditText mMessageEdit;
+    private TextView mResultText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSendButton = (Button) findViewById(R.id.send_button);
+        mHostEdit = (EditText) findViewById(R.id.host_edit_text);
+        mHostEdit.setText("192.168.1.63");
+        mPortEdit = (EditText) findViewById(R.id.port_edit_text);
+        mPortEdit.setText("50051");
+        mMessageEdit = (EditText) findViewById(R.id.message_edit_text);
+        mResultText = (TextView) findViewById(R.id.grpc_response_text);
+        mResultText.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void sendMessage(View view) {
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(mHostEdit.getWindowToken(), 0);
+        mSendButton.setEnabled(false);
+        new GrpcTask().execute();
+    }
+
+    private class GrpcTask extends AsyncTask<Void, Void, String> {
+        private String mHost;
+        private String mMessage;
+        private int mPort;
+        private ManagedChannel mChannel;
+
+        @Override
+        protected void onPreExecute() {
+            mHost = mHostEdit.getText().toString();
+            mMessage = mMessageEdit.getText().toString();
+            String portStr = mPortEdit.getText().toString();
+            mPort = TextUtils.isEmpty(portStr) ? 0 : Integer.valueOf(portStr);
+            mResultText.setText("");
+        }
+
+        @Override
+        protected String doInBackground(Void... nothing) {
+            try {
+                mChannel = ManagedChannelBuilder.forAddress(mHost, mPort)
+                        .usePlaintext(true)
+                        .build();
+                GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(mChannel);
+                HelloRequest message = HelloRequest.newBuilder().setName(mMessage).build();
+                HelloReply reply = stub.sayHello(message);
+                return reply.getMessage();
+            } catch (Exception e) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                pw.flush();
+                return String.format("Failed... : %n%s", sw);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                mChannel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            mResultText.setText(result);
+            mSendButton.setEnabled(true);
+        }
+    }
+}
